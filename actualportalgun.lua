eec6210f4a9e1c6d8918760124ddec1926dc3323
Netherite-Dev <114841605+NetheriteDeveloper@users.noreply.github.com>
2025-06-04 13:21:15 -0500

    Create actualportalgun.lua

---
+++ actualportalgun.lua
@@ -0,0 +1,13 @@
+if game.PlaceId == 3522803956 then -- Portal Stories Mel Portal Gun skin exclusive to the game, De Pride Isle Sanatorium, Eden Orphan Home, Equalia Falls Mental Insitute, Unit 1968, and Chicago 1949
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/NetheriteDeveloper/PersonalRobloxScriptsLibrary/refs/heads/main/src/portalgun'))()
+elseif game.PlaceId == 4786930269 then 
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/NetheriteDeveloper/PersonalRobloxScriptsLibrary/refs/heads/main/src/portalgun'))()
+elseif game.PlaceId == 3449573444 then 
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/randomguy194/dollar-tree-portal-gun/main/Special%20Skin%202'))()
+elseif game.PlaceId == 746820961 then 
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/NetheriteDeveloper/PersonalRobloxScriptsLibrary/refs/heads/main/src/portalgun'))()
+elseif game.PlaceId == 9675359772 then 
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/NetheriteDeveloper/PersonalRobloxScriptsLibrary/refs/heads/main/src/portalgun'))()
+else
+   loadstring(game:HttpGet('https://raw.githubusercontent.com/NetheriteDeveloper/PersonalRobloxScriptsLibrary/refs/heads/main/src/portalgun2'))()
+end
